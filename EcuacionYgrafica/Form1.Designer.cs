383e0037d501b8b2e9e1226f372e8802ba7aa8c3
eryka <eryka@LAPTOP-9IBP64BJ>
2022-05-30 19:38:02 -0500

    Agregar archivos de proyecto.

---
+++ EcuacionYgrafica/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace EcuacionYgrafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCal = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBoxform = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDatos = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonCal
+            // 
+            this.buttonCal.Location = new System.Drawing.Point(342, 29);
+            this.buttonCal.Name = "buttonCal";
+            this.buttonCal.Size = new System.Drawing.Size(75, 23);
+            this.buttonCal.TabIndex = 1;
+            this.buttonCal.Text = "Calcular";
+            this.buttonCal.UseVisualStyleBackColor = true;
+            this.buttonCal.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 72);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(536, 352);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBoxDatos);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(528, 323);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Datos";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(528, 323);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Grafica";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxform
+            // 
+            this.textBoxform.Location = new System.Drawing.Point(68, 26);
+            this.textBoxform.Name = "textBoxform";
+            this.textBoxform.Size = new System.Drawing.Size(259, 22);
+            this.textBoxform.TabIndex = 3;
+            this.textBoxform.Text = "X ^ 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Y=";
+            // 
+            // textBoxDatos
+            // 
+            this.textBoxDatos.Location = new System.Drawing.Point(26, 27);
+            this.textBoxDatos.Multiline = true;
+            this.textBoxDatos.Name = "textBoxDatos";
+            this.textBoxDatos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDatos.Size = new System.Drawing.Size(468, 269);
+            this.textBoxDatos.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(64, 17);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(420, 279);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxform);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.buttonCal);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCal;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBoxDatos;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBoxform;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EcuacionYgrafica/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace EcuacionYgrafica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cad= textBoxform.Text;
+            string[] tokens = cad.Split(' ');
+            //tokens[0] -> X
+            //tokens[1] -> *
+            //tokens[2] -> n
+
+            double rinix = -10.0;
+            //double rfinx = 10.0;
+            double y = 0.0;
+            if(tokens.Length == 3 )
+            {
+                double pot = Convert.ToDouble(tokens[2]);
+                int i;
+                inicializaDatos();
+                textBoxDatos.Text = "";
+                for (i = 1; i <= 20; i++)
+                {
+                    y = Math.Pow(rinix, pot);
+                    textBoxDatos.Text = textBoxDatos.Text + "    " + rinix.ToString()
+                     + "     " + y.ToString() + Environment.NewLine;
+                    rinix += 1.0;
+                    this.chart1.Series["ValorY"].Points.AddXY(rinix, y);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hay un error en la formula pelotudo", "Error");
+            }
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inicializaDatos();
+        }
+        void inicializaDatos()
+        {
+            //limpiar en char
+            this.chart1.Series.Clear();
+
+            //Crear la serie
+            //declarar un objeto que estara dentro
+            var serieAY = chart1.Series.Add("ValorY");
+            serieAY.ChartType = SeriesChartType.Spline;
+            serieAY.XValueType = ChartValueType.Single;
+            //asigna datos simples
+            //color
+            chart1.Series["ValorY"].Color = Color.Aqua;
+        }
+    }
+}
